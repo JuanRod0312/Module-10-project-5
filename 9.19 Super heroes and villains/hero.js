c14c1c25016afdf6da0aa883781914ee7eb45a0a
JuanRodFAU <157532708+JuanRod0312@users.noreply.github.com>
2024-07-19 15:19:03 -0400

    Created class Superhuman

---
+++ 9.19 Super heroes and villains/hero.js
@@ -0,0 +1,10 @@
+// TODO: Define SuperHuman, SuperHero, and SuperVillain classes here
+
+//SuperHuman class
+class SuperHuman {
+    constructor(name, powerLevel) 
+    {
+        this.name = name;
+        this.powerLevel = powerLevel;
+    }
+}
