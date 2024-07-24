b647acfbe4a798f5ba0e75bb912aaf8420399da8
JuanRodFAU <157532708+JuanRod0312@users.noreply.github.com>
2024-07-24 11:38:58 -0400

    Fetching an manupulating url

---
+++ 9.23 Quote web API/quote.js
@@ -0,0 +1,34 @@
+window.addEventListener("DOMContentLoaded", function () {
+   document.querySelector("#fetchQuotesBtn").addEventListener("click", function () {
+
+      // Get values from drop-downs
+      const topicDropdown = document.querySelector("#topicSelection");
+      const selectedTopic = topicDropdown.options[topicDropdown.selectedIndex].value;
+      const countDropdown = document.querySelector("#countSelection");
+      const selectedCount = countDropdown.options[countDropdown.selectedIndex].value;
+   
+      // Get and display quotes
+      fetchQuotes(selectedTopic, selectedCount);	   
+   });
+});
+
+function showAnonymousQuotes(count) {
+   let html = "<ol>";
+   for (let c = 1; c <= count; c++) {
+      html += `<li>Quote ${c} - Anonymous</li>`;
+   }
+   html += "</ol>";
+
+   document.querySelector("#quotes").innerHTML = html
+}
+
+function fetchQuotes(topic, count) {   
+   let url = "https://wp.zybooks.com/quotes.php?topic=" + encodeURIComponent(topic) + "&count=" + count;
+   fetch(url)
+      .then(response => response.json())
+      .then(data => {
+         if (data.error) {
+            document.querySelector("#quotes").innerHTML = data.error;
+         } 
+      })
+}
